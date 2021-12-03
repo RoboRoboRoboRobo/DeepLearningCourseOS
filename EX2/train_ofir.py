eb8b6b9346832744611956cb616fd6dd8d3d11f4
ofir-kr <ofir.krauz@gmail.com>
2021-12-03 18:37:15 +0200

    Adding Ofir's version - 2nd try

--- EX2/train_ofir.py
+++ EX2/train_ofir.py
@@ -2,36 +2,32 @@ import torch
 from torch import nn
 from torch.autograd import Variable
 from torch import optim
-
-def evaluateModel(dataset, model, embed):
-
+import numpy as np
 
 
-    #### TODOD REMOVE
+def evaluateModel(dataset, model, criterion, device):
+    with torch.no_grad():
+        # losses = []
+        loss = 0
+        for i, (x, y) in enumerate(dataset):
+            outputs = model(x)
+            outputs = outputs.reshape(-1, outputs.shape[-1])
+            outputs = Variable(outputs).to(device)
+            y = y.reshape(-1)
+            loss += torch.exp(criterion(outputs, y))
+            if i % 500 == 0:
+                print(f"Finished batch number {i}/{len(dataset)} in evaluation")
 
-    dataset = dataset[:2]
+        return loss / len(dataset)
 
-    ####
 
-    num_batches = len(dataset)
-    batch_size = dataset[0][0].shape[0]
-    seq_length = dataset[0][0].shape[1]
-    vocab_size = embed.vocab_size
-    tot_x = torch.zeros([num_batches * batch_size * seq_length, vocab_size])
-    tot_y = torch.zeros(tot_x.shape[0])
-    for i in range(0, num_batches, batch_size*seq_length):
-        (x, y) = dataset[i]
-        tot_x[i:i + batch_size*seq_length,:] = model(embed(x)).reshape(-1, vocab_size)
-        tot_y[i:i + batch_size*seq_length] = y.reshape(-1)
-    tot_x = embed(tot_x)
-    output = model(tot_x)
-
-def train(model, trn_dataset, val_dataset, tst_dataset, embed, device,
-          variation, optimizer, epoch_num, checkpoints_dir_path, latest_checkpoint_path=""):
-
+def train(model, trn_dataset, val_dataset, tst_dataset, batch_size, sequence_length, lr, lr_factor, lr_change_epoch,
+          max_grad_norm, embed, device, variation, optimizer, epoch_num, checkpoints_dir_path, writer,
+          latest_checkpoint_path=""):
     train_acc_list = []
     test_acc_list = []
     epoch_list = []
+    criterion = nn.CrossEntropyLoss()
 
     # checkpoints handling
     if latest_checkpoint_path == "":
@@ -47,127 +43,81 @@ def train(model, trn_dataset, val_dataset, tst_dataset, embed, device,
     epochs_range = range(first_epoch, epoch_num)
     print(f"epochs_range = {epochs_range}")
 
-    perplexity_trn = evaluateModel(trn_dataset, model, embed)
     # epoch loop
     for e in epochs_range:
+        model.train()
+        if e >= lr_change_epoch:
+            lr /= lr_factor
+            for g in optimizer.param_groups:
+                g['lr'] = lr
         running_loss = 0
 
         # batch loop
-        for (x, y) in trn_dataset:
+        # for (x, y) in trn_dataset:
+        for i, (x, y) in enumerate(trn_dataset):  ## TODO move back
             # move input and output to GPU
             x = Variable(x).to(device)
             y = Variable(y).to(device)
 
             # clear gradients prior to new batch (since gradients are accumulated)
-            optimizer.zero_grad()
+            # optimizer.zero_grad()
 
             # obtain output probabilities (by feed forward images batch across the model)
-            x = embed(x) ## seq_len x batch_size x word_vec_size (35x20x200)
+            # x = embed(x) ## seq_len x batch_size x word_vec_size (35x20x200)
             # y = embed(y) ## seq_len x batch_size x word_vec_size (35x20x200)
-
-            x = model(x) ## tensor in the size of vocab_size over batch size (20 x 1 x 10,000)
+            model.zero_grad()
+            x = model(x)  ## tensor in the size of vocab_size over batch size (20 x 1 x 10,000)
 
             # obtain loss
             x = torch.exp(x)
-            x = x.view(-1, vocab_size)
+            x = x.view(-1, model.embed.vocab_size)
             y = y.reshape(batch_size * sequence_length)
-            criterion = nn.CrossEntropyLoss()
             loss = criterion(x, y)
 
             # compute gradients
             loss.backward()
 
             # update weights
-            optimizer.step()
+            with torch.no_grad():
+                norm = nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+                for param in model.parameters():
+                    if param is None:
+                        param -= lr * param.grad
 
             # accumulate loss
             running_loss += loss.item()
 
+            if i % 500 == 0 and i > 0:
+                print("dw.norm() = {:.3f}, ".format(norm))
+                print(f"Train : batch num: {i}/{len(trn_dataset)} loss: {running_loss / i}")
+
         # deactivate dropout and batch normalization
-        if ('DO' == variation):
+        if ('DO' in variation):
             model.eval()
             # compute accuracies
-        perplexity_trn = evaluateModel(trn_dataset, model)
-        perplexity_val = evaluateModel(val_dataset, model)
-        perplexity_tst = evaluateModel(tst_dataset, model)
+        ## TODO consider eval
+        perplexity_trn = evaluateModel(trn_dataset, model, criterion, device)
+        perplexity_val = evaluateModel(val_dataset, model, criterion, device)
+        perplexity_tst = evaluateModel(tst_dataset, model, criterion, device)
         #
-        # # accumulate accuracies
-        # train_acc_list.append(acc_train)
-        # test_acc_list.append(acc_test)
-        # epoch_list.append(e)
-        #
-        # # save checkpint at the end of each epoch
-        # curr_checkpoint_path = checkpoints_dir_path + f'/Lenet5-{variation}-{e}.pth'
-        # print(curr_checkpoint_path)
-        # torch.save({'epoch': e, 'model_state_dict': model.state_dict(),
-        #             'optimizer_state_dict': optimizer.state_dict(),
-        #             'loss': running_loss / len(data_loader_train), },
-        #            curr_checkpoint_path)
-        # writer.add_scalars(f'{variation}', {
-        #     'Train Acc': acc_train,
-        #     'Test Acc': acc_test
-        # }, e)
+        # save checkpoint at the end of each epoch
+        curr_checkpoint_path = checkpoints_dir_path + f'/Zaremba-{variation}-{e}.pth'
+        print(curr_checkpoint_path)
+        torch.save({'epoch': e, 'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': running_loss / len(trn_dataset), },
+                   curr_checkpoint_path)
+        writer.add_scalars(f'{variation}', {
+            'Train Perplexity': perplexity_trn,
+            'Val Perplexity': perplexity_val,
+            'Test Perplexity': perplexity_tst
+        }, e)
 
-        # save train and test accuracies figure after last epoch
-        # if e == epoch_num - 1:
-        #
-        #     plt.figure()
-        #     plt.plot(epoch_list, train_acc_list, label='Train Acc')
-        #     plt.plot(epoch_list, test_acc_list, label='Test Acc')
-        #     plt.xlabel("epochs")
-        #     plt.ylabel("accuracy")
-        #     var_type = "Original"
-        #     if "DO" in variation:
-        #         var_type = "Dropout"
-        #     elif "WD" in variation:
-        #         var_type = "Weight decay"
-        #     elif "BN" in variation:
-        #         var_type = "Batch Normalization"
-        #     plt.title(f"Lenet5 - {var_type}")
-        #     plt.legend()
-        #     date_time = datetime.today().strftime('%d_%m_%Y_%H_%M_%S')
-        #     str_figure = figures_dir_path + f'{variation}-{e}-{date_time}.png'
-        #     plt.savefig(str_figure)
-        #
-        # # print states at the end of each epoch
-        # print(f"Epoch {e}:")
-        # print(f"Training loss:     {running_loss / len(data_loader_train)}")
-        # print(f"Training accuracy: {acc_train}")
-        # print(f"Test accuracy:     {acc_test}")
-        # print(f"-----------------------------------------------")
-
-
-
-
-## in notebook
-from model import Zaremba, Embedding ## move to notebook
-from preprocess import preprocess_ptb_files, create_dataset
-import torch
-word_vec_size = 200
-vocab_size = 10000 ## need to be calculated
-num_layers = 2
-batch_size = 20
-sequence_length = 35
-lr = 0.001
-epoch_num = 100
-variation = 'LSTM_no_DO'
-checkpoints_dir_path = '/Users/shir.barzel/DeepLearningCourseOS/EX2/results'
-trn_data, val_data, tst_data = preprocess_ptb_files('/Users/shir.barzel/DeepLearningCourseOS/EX2/PTB/ptb.char.train.txt',
-                     '/Users/shir.barzel/DeepLearningCourseOS/EX2/PTB/ptb.char.valid.txt',
-                     '/Users/shir.barzel/DeepLearningCourseOS/EX2/PTB/ptb.char.test.txt') ## TODO
-
-trn_dataset = create_dataset(trn_data, batch_size, sequence_length)
-val_dataset = create_dataset(val_data, batch_size, sequence_length)
-tst_dataset = create_dataset(tst_data, batch_size, sequence_length)
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(device)
-
-model = Zaremba(word_vec_size, word_vec_size, vocab_size, num_layers, variation)
-embed = Embedding(vocab_size, word_vec_size)
-optimizer = optim.Adam(params = model.parameters(), lr=lr, weight_decay=0)
-
-train(model, trn_dataset, val_dataset, tst_dataset,
-      embed, device, variation, optimizer, epoch_num, checkpoints_dir_path)
-## in notebook
+        # print states at the end of each epoch
+        print(f"Epoch {e}:")
+        print(f"Training loss:     {running_loss / len(trn_dataset)}")
+        print(f"Training Perplexity: {perplexity_trn}")
+        print(f"Val Perplexity:      {perplexity_val}")
+        print(f"Test Perplexity:     {perplexity_tst}")
+        print(f"-----------------------------------------------")
 
