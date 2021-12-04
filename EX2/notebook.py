Update notebook and fix issue with gradients

EX2/notebook.py
@@ -1,9 +1,11 @@
-from model import Zaremba, Embedding ## move to notebook
+from model import Zaremba, Embedding
 from preprocess import preprocess_ptb_files, create_dataset
 import torch
 from torch import optim
 from torch.utils.tensorboard import SummaryWriter
 from train import train
+from datetime import datetime
+import os
 
 word_vec_size = 200
 vocab_size = 10000  ## need to be calculated
@@ -21,7 +23,8 @@ variation = 'LSTM'
 OFIR   C:\\Users\ofir-kr\PycharmProjects\DeepLearningCourseOS\EX2\\"
 SHIR   /Users/shir.barzel/DeepLearningCourseOS/EX2/
 """
-user = "Ofir"
+user = "Shir"
+
 if user == "Ofir":
     slash ="\\"
     ptb_dir = "C:\\Users\\ofir-kr\\PycharmProjects\\DeepLearningCourseOS\\EX2\PTB\\"
@@ -43,15 +46,15 @@ trn_dataset = create_dataset(trn_data, batch_size, sequence_length, device)
 val_dataset = create_dataset(val_data, batch_size, sequence_length, device)
 tst_dataset = create_dataset(tst_data, batch_size, sequence_length, device)
 
-# now = datetime.now()
-# time = now.strftime('%d_%m_%y_%H_%M')
-# path_results_dir = f'{root_path}/ex2_301917670_302921366/results/{time}'
-# if not os.path.exists(path_results_dir):
-#   os.makedirs(f'{path_results_dir}/checkpoints')
-#   os.makedirs(f'{path_results_dir}/events')
+now = datetime.now()
+time = now.strftime('%d_%m_%y_%H_%M')
+checkpoints_dir_path = f'{checkpoints_dir_path}/{time}'
+if not os.path.exists(checkpoints_dir_path):
+  os.makedirs(f'{checkpoints_dir_path}/checkpoints')
+  os.makedirs(f'{checkpoints_dir_path}/events')
 
-# events_dir = checkpoints_dir_path + slash + 'events' + slash
-# writer = SummaryWriter(events_dir)
+events_dir = checkpoints_dir_path + slash + 'events' + slash
+writer = SummaryWriter(events_dir)
 
 model = Zaremba(word_vec_size, word_vec_size, vocab_size, num_layers, variation)
 embed = Embedding(vocab_size, word_vec_size)
@@ -60,4 +63,4 @@ optimizer = optim.Adam(params=model.parameters(), lr=lr, weight_decay=0)
 model.to(device)
 print("starting train")
 train(model, trn_dataset, val_dataset, tst_dataset, batch_size, sequence_length, lr, lr_factor, lr_change_epoch,
-      max_grad_norm, embed, device, variation, optimizer, epoch_num, checkpoints_dir_path, writer=0)
+      max_grad_norm, embed, device, variation, optimizer, epoch_num, checkpoints_dir_path, writer)

EX2/train.py
@@ -4,47 +4,22 @@ from torch.autograd import Variable
 from torch import optim
 import numpy as np
 
-''' ORIGINAL evaluateModel '''
-# def evaluateModel(dataset, model, criterion, device):
-#     with torch.no_grad():
-#         # losses = []
-#         loss = 0
-#         for i, (x, y) in enumerate(dataset):
-#             outputs = model(x)
-#             outputs = outputs.reshape(-1, outputs.shape[-1])
-#             outputs = Variable(outputs).to(device)
-#             y = y.reshape(-1)
-#             loss += torch.exp(criterion(outputs, y))
-#             if i % 500 == 0:
-#                 print(f"Finished batch number {i}/{len(dataset)} in evaluation")
-#
-#         return loss / len(dataset)
-
-# REFERENCE nll_loss
 def nll_loss(scores, y):
     batch_size = y.size(1)
     scores = torch.reshape(scores, (y.numel(), -1))
     expscores = scores.exp()
     probabilities = expscores / expscores.sum(1, keepdim=True)
     answerprobs = probabilities[range(len(y.reshape(-1))), y.reshape(-1)]
-    #I multiply by batch_size as in the original paper
-    #Zaremba et al. sum the loss over batches but average these over time.
     return torch.mean(-torch.log(answerprobs) * batch_size)
 
-
-# REFERENCE perplexity -> OURS evaluateModel
 def evaluateModel(data, model, batch_size, device):
     with torch.no_grad():
         loss = 0
-        # states = model.state_init(batch_size) # OURS doesn't use states
         for x, y in data:
-            # scores, states = model(x, states) # ORIGINAL
             scores = model(x)  # scores <-> outputs
             scores = Variable(scores).to(device)
             loss += nll_loss(scores, y)
-            #Again with the sum/average implementation described in 'nll_loss'.
     return torch.exp(loss/(batch_size*len(data)))
-
 
 def train(model, trn_dataset, val_dataset, tst_dataset, batch_size, sequence_length, lr, lr_factor, lr_change_epoch,
           max_grad_norm, embed, device, variation, optimizer, epoch_num, checkpoints_dir_path, writer,
@@ -76,65 +51,37 @@ def train(model, trn_dataset, val_dataset, tst_dataset, batch_size, sequence_len
             for g in optimizer.param_groups:
                 g['lr'] = lr
         running_loss = 0
-
         # batch loop
-        for i, (x, y) in enumerate(trn_dataset):  ## TODO move back
+        for i, (x, y) in enumerate(trn_dataset):
             # move input and output to GPU
             x = Variable(x).to(device)
-            y = Variable(y, requires_grad=True).to(device)
-
-            # clear gradients prior to new batch (since gradients are accumulated)
-            # optimizer.zero_grad()
-
-            # obtain output probabilities (by feed forward images batch across the model)
-            # x = embed(x) ## seq_len x batch_size x word_vec_size (35x20x200)
-            # y = embed(y) ## seq_len x batch_size x word_vec_size (35x20x200)
+            y = Variable(y).to(device)
             model.zero_grad()
             scores = model(x)  ## tensor in the size of vocab_size over batch size (20 x 1 x 10,000)
-            scores = Variable(scores).to(device)
-
-            # obtain loss
-
-            # exp_scores = torch.exp(scores)
-            # exp_scores = exp_scores.view(-1, model.embed.vocab_size)
-            # y = y.reshape(batch_size * sequence_length)
-            # loss = criterion(exp_scores, y)
-
-            ''' checking REF '''
-            scores = Variable(scores, requires_grad=True).to(device)
             loss = nll_loss(scores, y)
-            # loss = Variable(loss, requires_grad=True)
-
             # compute gradients
             loss.backward()
 
-            # update weights
             with torch.no_grad():
                 norm = nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
                 for param in model.parameters():
-                    if param is None:
+                    if not param.grad is None:
                         param -= lr * param.grad
 
             # accumulate loss
             running_loss += loss.item()
 
             if i % 500 == 0 and i > 0:
-                print("dw.norm() = {:.3f}, ".format(norm))
-                print(f"Train : batch num: {i}/{len(trn_dataset)} loss: {running_loss / i}")
+                print("Grad norm = {:.3f}, ".format(norm))
+                print(f"Train : Batch num: {i}/{len(trn_dataset)}, Loss: {running_loss / i}")
 
         # deactivate dropout and batch normalization
         if ('DO' in variation):
             model.eval()
             # compute accuracies
-        ## TODO consider eval
-        # perplexity_trn = evaluateModel(trn_dataset, model, criterion, device)
-        # perplexity_val = evaluateModel(val_dataset, model, criterion, device)
-        # perplexity_tst = evaluateModel(tst_dataset, model, criterion, device)
-        ''' checking REF'''
         perplexity_trn = evaluateModel(trn_dataset, model, batch_size, device)
         perplexity_val = evaluateModel(val_dataset, model, batch_size, device)
         perplexity_tst = evaluateModel(tst_dataset, model, batch_size, device)
-        #
         # save checkpoint at the end of each epoch
         curr_checkpoint_path = checkpoints_dir_path + f'/Zaremba-{variation}-{e}.pth'
         print(curr_checkpoint_path)
@@ -142,10 +89,11 @@ def train(model, trn_dataset, val_dataset, tst_dataset, batch_size, sequence_len
                     'optimizer_state_dict': optimizer.state_dict(),
                     'loss': running_loss / len(trn_dataset), },
                    curr_checkpoint_path)
+
         writer.add_scalars(f'{variation}', {
-            'Train Perplexity': perplexity_trn,
-            'Val Perplexity': perplexity_val,
-            'Test Perplexity': perplexity_tst
+            'Train_Perplexity': float(perplexity_trn),
+            'Val_Perplexity': float(perplexity_val),
+            'Test_Perplexity': float(perplexity_tst)
         }, e)
 
         # print states at the end of each epoch