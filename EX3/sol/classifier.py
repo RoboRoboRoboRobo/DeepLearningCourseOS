2348fd1da22a7ad6e305e73700fda9f49b210d83
shir.barzel <shir.barzel@healthy.io>
2021-12-29 19:32:02 +0200

    Remove comments

--- EX3/sol/classifier.py
+++ EX3/sol/classifier.py
@@ -2,30 +2,6 @@ import torch
 from sklearn import svm
 import numpy as np
 
-## TODO Remove?
-# numDataPoints = 1000
-# data_dim = 5
-# bs = 100
-#
-# # Create dummy data with class imbalance 9 to 1
-# data = torch.FloatTensor(numDataPoints, data_dim)
-# target = np.hstack((np.zeros(int(numDataPoints * 0.9), dtype=np.int32),
-#                     np.ones(int(numDataPoints * 0.1), dtype=np.int32)))
-#
-# print('target train 0/1: {}/{}'.format(len(np.where(target == 0)[0]), len(np.where(target == 1)[0])))
-#
-# class_sample_count = np.array(
-#     [len(np.where(target == t)[0]) for t in np.unique(target)])
-# weight = 1. / class_sample_count
-# samples_weight = np.array([weight[t] for t in target])
-#
-# samples_weight = torch.from_numpy(samples_weight)
-# samples_weigth = samples_weight.double()
-# sampler = torch.utils.data.WeightedRandomSampler(samples_weight, len(samples_weight))
-#
-# target = torch.from_numpy(target).long()
-# train_dataset = torch.utils.data.TensorDataset(data, target)
-
 def train_classifier(encoder, trn_dataset, number_of_labeled_samples, num_of_classes):
 
     x_train = []
